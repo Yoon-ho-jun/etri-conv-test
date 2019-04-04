5dc284e387a11277c26c0b91b72087af3f0cf04a
Yoon-ho-jun <62797106@hanmail.net>
2019-04-04 13:48:24 +0900

    updated on day 3

--- conv_diy.cpp
+++ conv_diy.cpp
@@ -2,28 +2,48 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
-int your_conv( cv::Mat src,
-               cv::Mat dst,
-               cv::Mat kernel,
+int your_conv( cv::Mat src,  //matrix
+               cv::Mat dst,  //output
+               cv::Mat kernel, //filter
                int stride,
                int padding
                )
 {
     int src_height = src.rows;
     int src_width = src.cols;
+   
 
-    int kernel_height = kernel.rows;
+    int x,y,i,j,k;
+
+    int kernel_hieght = kernel.rows;
     int kernel_width = kernel.cols;
+    
 
     int dst_height;
     int dst_width;
-
-    // src.ptr<unsigned char>(i)[ calculate INDEX ]
+    
+    dst_height = (src_height + 2 * padding - kernel_hieght)/stride +1;
+    dst_width = (src_width + 2 * padding - kernel_width)/stride +1;
+    // src.ptr<unsigned char>(i)[ calculate INDEX]
 
     // MAKE YOUR OWN CONVOLUTION PROCESS
 
+    for(x=0; x<dst_height; x++){
+	for(y=0; y<dst_width; y++){
+
+		for(i=0; i<dst_height; i++){
+			for(j=0; j<dst_width; j++){
+				for(k=0; k<3; k++)
+					dst.at<float>(x,y) += (src.at<float>(i,j,k) * kernel.at<float>(i,j,k));		
+			}
+		}
+	}
+    }
+				
+	
+	
     // if success
-    return 0
+    return 0;
 
     // if fail - in the case of wrong parameters...
     // return -1
@@ -37,7 +57,7 @@ int main ( int argc, char** argv )
         return -1;
     }
 
-    cv::Mat src, kernel, dst;
+    cv::Mat src, kernel, dst; 
 
     // Load an image
     src = cv::imread( argv[1] );
